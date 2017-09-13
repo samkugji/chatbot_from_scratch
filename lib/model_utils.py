c8da2aa71a7f59c403ce5eb866806cc1577732f2
samkugji <samkugji@gmail.com>
2017-09-13 17:00:14 +0900

    update lib files

---
+++ lib/model_utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.python.platform import gfile
+from configs import model_config
+from lib import chat_seq2seq_model_old, data_utils
+
+
+def create_model(session, config, forward_only=False):
+    model = chat_seq2seq_model_old.ChatSeq2SeqModel(
+        config=config,
+        forward_only=forward_only
+    )
+
+    ckpt = tf.train.get_checkpoint_state(config.model_dir)
+    # if model files existed, load model file
+    if ckpt and gfile.Exists("{}.index".format(ckpt.model_checkpoint_path)):
+        print("Reading model parameters from {}".format(ckpt.model_checkpoint_path))
+        model.saver.restore(session, ckpt.model_checkpoint_path)
+    # initialize parameters and create new mode
+    else:
+        print("Created model with fresh parameters.")
+        session.run(tf.global_variables_initializer())
+
+    return model
